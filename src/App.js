dc6e4c8349a47d4a58639c9ff326c292e183d726
Nathaniel <nathanielgumm@gmail.com>
2025-08-06 16:17:08 -0700

    Responsive coming soon page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ShopPage from './pages/ShopPage';
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ShopPage />} />
-        {/* We'll add /product/:id route soon */}
-      </Routes>
-    </Router>
+    <div className="relative w-screen h-screen">
+      <img
+        src="/images/coming-soon.jpg"
+        alt="Coming Soon"
+        className="w-full h-full object-cover"
+      />
+
+      <div className="absolute inset-0 bg-black bg-opacity-60 flex flex-col items-center justify-center text-white text-center px-4">
+        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4">
+          CFR Shirts
+        </h1>
+        <p className="text-lg sm:text-xl lg:text-2xl">
+          Coming Soon. Gear up for launch!
+        </p>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
